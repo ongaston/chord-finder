13f25207154796ac844ca847af41c2ac00f64999
Olivia Gaston <goldfishwithsalsa@gmail.com>
2022-08-11 11:18:10 -0500

    slidey mode expansion

--- resources/scripts/dropdowns.js
+++ resources/scripts/dropdowns.js
@@ -1,5 +1,6 @@
 import { noteArray, Ab, A, Bb, B, C, Csharp, Db, D, Eb, E, F, Fsharp, Gb, G, I, ii, II, iii, III, PIV, tritone, PV, vi, VI, vii, VII, Note, letterArray, addIntervals } from './intervals.js';
 
+/* #region  Variables */
 let seventhContainerTitle = document.getElementById('seventh-container');
 let seventhChordsContainer = document.getElementById('seventh-chords-container');
 let seventhChordsContent = document.getElementsByClassName('seventh');
@@ -31,77 +32,115 @@ let miscChordsContainer = document.getElementById('misc-scale-container');
 
 let modesContainerTitle = document.getElementById('mode-title');
 let modeContainer = document.getElementById('modes-dropdown-container');
+let bigContainer = document.getElementById('big-container');
 
-$(function() {
+let scalesContainerWidth = $(bigContainer).width();
+scalesContainerWidth = Math.floor(scalesContainerWidth);
+scalesContainerWidth = scalesContainerWidth - 32;
+scalesContainerWidth = scalesContainerWidth.toString().concat('', 'px');
+let bigContainerWidth = ($(bigContainer).width())
+let scaleWidth = Math.floor(bigContainerWidth) - 32;
 
-$(window).on('load', function() {
-    for (let i = 0; i < seventhChordsContent.length; i++) {
-        $(seventhChordsContent[i]).css('display', 'hidden');
+let toggleLength = true;
 
-    }
-    for (let i = 0; i < sixthChordsContent.length; i++) {
-        $(sixthChordsContent[i]).css('display', 'hidden');
-    }
-    for (let i = 0; i < ninthChordsContent.length; i++) {
-        $(ninthChordsContent[i]).css('display', 'hidden');
-    }
-    for (let i = 0; i < susChordsContent.length; i++) {
-        $(susChordsContent[i]).css('display', 'hidden');
-    }
-    for (let i = 0; i < otherChordsContent.length; i++) {
-        $(otherChordsContent[i]).css('display', 'hidden');
-    }
-})
+let chordContainer = document.getElementById('chord-container');
 
-$(modesContainerTitle).on('click', function() {
-    $(modeContainer).slideToggle();
-})
+let modeWidth = scaleWidth / bigContainerWidth;
+modeWidth = Math.floor(modeWidth * 100 + 64);
+modeWidth = modeWidth.toString().concat('', 'px');
 
-$(miscContainerTitle).on('click', function() {
-    $(miscChordsContainer).slideToggle();
-})
+/* #endregion */
 
-$(minorContainerTitle).on('click', function() {
-    $(minorChordsContainer).slideToggle();
-})
 
-$(pentatonicContainerTitle).on('click', function() {
-    $(pentatonicChordsContainer).slideToggle();
-})
+$(chordContainer).css('width', scalesContainerWidth);
 
-$(seventhContainerTitle).on('click', function() {
-    $(seventhChordsContainer).slideToggle();
-    for (let i = 0; i < seventhChordsContent.length; i++) {
-        $(seventhChordsContent[i]).css('display', 'block');
-    }
-});
+$(function () {
 
-$(sixthContainerTitle).on('click', function() {
-    $(sixthChordsContainer).slideToggle();
-    for (let i = 0; i < sixthChordsContent.length; i++) {
-        $(sixthChordsContent[i]).css('display', 'block');
-    }
-})
+    $(window).on('load', function () {
+        for (let i = 0; i < seventhChordsContent.length; i++) {
+            $(seventhChordsContent[i]).css('display', 'hidden');
 
-$(ninthContainerTitle).on('click', function() {
-    $(ninthChordsContainer).slideToggle();
-    for (let i = 0; i < ninthChordsContent.length; i++) {
-        $(ninthChordsContent[i]).css('display', 'block');
-    }
-})
+        }
+        for (let i = 0; i < sixthChordsContent.length; i++) {
+            $(sixthChordsContent[i]).css('display', 'hidden');
+        }
+        for (let i = 0; i < ninthChordsContent.length; i++) {
+            $(ninthChordsContent[i]).css('display', 'hidden');
+        }
+        for (let i = 0; i < susChordsContent.length; i++) {
+            $(susChordsContent[i]).css('display', 'hidden');
+        }
+        for (let i = 0; i < otherChordsContent.length; i++) {
+            $(otherChordsContent[i]).css('display', 'hidden');
+        }
+    })
 
-$(susContainerTitle).on('click', function() {
-    $(susChordsContainer).slideToggle();
-    for (let i = 0; i < susChordsContent.length; i++) {
-        $(susChordsContent[i]).css('display', 'block');
-    }
-})
+    $(modesContainerTitle).on('click', function () {
 
-$(otherContainerTitle).on('click', function() {
-    $(otherChordsContainer).slideToggle();
-    for (let i = 0; i < otherChordsContent.length; i++) {
-        $(otherChordsContent[i]).css('display', 'block');
-    }
-})
+        if (toggleLength) {
+            toggleLength = false;
+            $(modesContainerTitle).animate({
+                width: scalesContainerWidth
+            }, 300);
+            $(modeContainer).delay(300).slideToggle();
+        } else if (!toggleLength) {
+            $(modeContainer).slideToggle();
+            $(modesContainerTitle).delay(400).animate({
+                width: modeWidth
+            }, 300);
+            toggleLength = true;
+
+        }
+
+    })
+
+
+
+    $(miscContainerTitle).on('click', function () {
+        $(miscChordsContainer).slideToggle();
+    })
+
+    $(minorContainerTitle).on('click', function () {
+        $(minorChordsContainer).slideToggle();
+    })
+
+    $(pentatonicContainerTitle).on('click', function () {
+        $(pentatonicChordsContainer).slideToggle();
+    })
+
+    $(seventhContainerTitle).on('click', function () {
+        $(seventhChordsContainer).slideToggle();
+        for (let i = 0; i < seventhChordsContent.length; i++) {
+            $(seventhChordsContent[i]).css('display', 'block');
+        }
+    });
+
+    $(sixthContainerTitle).on('click', function () {
+        $(sixthChordsContainer).slideToggle();
+        for (let i = 0; i < sixthChordsContent.length; i++) {
+            $(sixthChordsContent[i]).css('display', 'block');
+        }
+    })
+
+    $(ninthContainerTitle).on('click', function () {
+        $(ninthChordsContainer).slideToggle();
+        for (let i = 0; i < ninthChordsContent.length; i++) {
+            $(ninthChordsContent[i]).css('display', 'block');
+        }
+    })
+
+    $(susContainerTitle).on('click', function () {
+        $(susChordsContainer).slideToggle();
+        for (let i = 0; i < susChordsContent.length; i++) {
+            $(susChordsContent[i]).css('display', 'block');
+        }
+    })
+
+    $(otherContainerTitle).on('click', function () {
+        $(otherChordsContainer).slideToggle();
+        for (let i = 0; i < otherChordsContent.length; i++) {
+            $(otherChordsContent[i]).css('display', 'block');
+        }
+    })
 
 });
